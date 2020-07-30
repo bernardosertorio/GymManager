8766f4ddf26740424e3d15fba63127811ef4549c
Bernardo Sertório <besertorio@gmail.com>
2020-07-30 16:52:46 -0300

    :construction: paging logic

--- public/scripts.js
+++ public/scripts.js
@@ -9,3 +9,29 @@ for (item of menuItens) {
         item.classList.add("active")
     }
 }
+
+let totalPages = 20,
+    selectedPage = 15,
+    pages = [],
+    oldPage
+
+for(let currentPage = 1; currentPage <= totalPages; currentPage++) {
+
+    const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+    const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+    const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
+
+    if (firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+        if (oldPage && currentPage - oldPage > 2) {
+            pages.push("...")
+        }
+
+        if (oldPage && currentPage - oldPage == 2) {
+            pages.push(oldPage + 1)
+        }
+
+        pages.push(currentPage)
+
+        oldPage = currentPage
+    }
+}
