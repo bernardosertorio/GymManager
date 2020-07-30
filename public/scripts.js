30a1749bcfefa4663fa37e967b01f401b1dd38f4
Bernardo Sertório <besertorio@gmail.com>
2020-07-30 17:48:06 -0300

    :construction: Pagination logic into function

--- public/scripts.js
+++ public/scripts.js
@@ -10,28 +10,33 @@ for (item of menuItens) {
     }
 }
 
-let totalPages = 20,
-    selectedPage = 15,
-    pages = [],
-    oldPage
+// Lógica de paginação
 
-for(let currentPage = 1; currentPage <= totalPages; currentPage++) {
+function pagination(selectedPage, totalPages) {
 
-    const firstAndLastPage = currentPage == 1 || currentPage == totalPages
-    const pagesAfterSelectedPage = currentPage <= selectedPage + 2
-    const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
+    let pages = [],
+        oldPage
 
-    if (firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
-        if (oldPage && currentPage - oldPage > 2) {
-            pages.push("...")
+    for(let currentPage = 1; currentPage <= totalPages; currentPage++) {
+
+        const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+        const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+        const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
+
+        if (firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+            if (oldPage && currentPage - oldPage > 2) {
+                pages.push("...")
+            }
+
+            if (oldPage && currentPage - oldPage == 2) {
+                pages.push(oldPage + 1)
+            }
+
+            pages.push(currentPage)
+
+            oldPage = currentPage
         }
-
-        if (oldPage && currentPage - oldPage == 2) {
-            pages.push(oldPage + 1)
-        }
-
-        pages.push(currentPage)
-
-        oldPage = currentPage
     }
+
+    return pages
 }
