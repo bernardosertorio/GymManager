d9fc364627cc57cd8618344b85ddf147aba4e855
Bernardo Sertório <besertorio@gmail.com>
2020-07-31 13:11:44 -0300

    :construction: Editing query and page logic

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -19,6 +19,12 @@ module.exports = {
       offset,
       callback(instructors) {
 
+        const pagination = {
+          total: Math.ceil(instructors[0].total / limit),
+          page
+        }
+
+        return res.render("instructors/index", { instructors, pagination, filter})
       }
     }
 
